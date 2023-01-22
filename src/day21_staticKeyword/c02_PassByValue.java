ee70567d6e254879c3a8f10d00399f4ff0a2642a
sadettinak <sadettinakman@gmail.com>
2023-01-22 23:38:39 +0300

    a

--- src/day21_staticKeyword/c02_PassByValue.java
+++ src/day21_staticKeyword/c02_PassByValue.java
@@ -4,5 +4,13 @@ public class c02_PassByValue {
     public static void main(String[] args) {
         int fiyat=100;
         System.out.println(fiyat);
+        int fiyat1=150;
+        System.out.println(fiyat1);
+        //method2(5);
+        String s= "Hasan";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
     }
 }
